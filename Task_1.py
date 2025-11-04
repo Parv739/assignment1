c1066a7ab48548f43b08db4ae4aea79e38e24466
Parv739 <parvb5358@gmail.com>
2025-11-04 20:59:55 +0530

    Uploaded Assignment files

---
+++ Task_1.py
@@ -0,0 +1,9 @@
+# Task 1 - Take input from user and perform mathematical operations.
+
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+
+print(f"Addition : ", num1 + num2)
+print(f"Subtraction : ", num1 - num2)
+print(f"Multiplication : ", num1 * num2)
+print(f"Division : ", num1 / num2)
